4c98a79098ba4ec71a100a515843858be643e396
Lamine <lemzoba@gmail.com>
2016-12-15 17:08:41 +0100

    Done the second tutorial of rabbitmq on the official website

---
+++ part_II/worker.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import time
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
