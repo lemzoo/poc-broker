260415ce92b9b6c47dbabd83d04a578cb3d845a0
Lamine <lemzoba@gmail.com>
2016-12-15 17:07:17 +0100

    Renaming the file with extension .cmd to .sh

---
+++ rabbitmq.sh
@@ -0,0 +1,50 @@
+# http://docs.celeryproject.org/en/latest/getting-started/brokers/rabbitmq.html
+
+# Starting/Stopping the RabbitMQ server
+
+# To start the server:
+$ sudo rabbitmq-server
+
+# You can also run it in the background by adding the
+# -detached option (note: only one dash):
+$ sudo rabbitmq-server -detached
+
+# Never use kill (kill(1)) to stop the RabbitMQ server,
+# but rather use the rabbitmqctl command:
+$ sudo rabbitmqctl stop
+
+# To delete all the message on the queue
+$ sudo rabbitmqctl purge_queue queue_name
+
+# To list all the queue and see process
+$ sudo rabbitmqctl list_queues name messages_ready messages_unacknowledged
+
+# Activate rabbitmq plugins
+$ sudo rabbitmq-plugins enable rabbitmq_management
+
+# Acces to the interface
+http://localhost:15672/
+username :
+password :
+
+# interface admin ref
+https://www.rabbitmq.com/management.html
+
+###############################################################
+################### Setting up RabbitMQ #######################
+###############################################################
+# To use Celery we need to create a RabbitMQ user,
+# a virtual host and allow that user access to that virtual host:
+
+$ sudo rabbitmqctl add_user myuser mypassword
+$ sudo rabbitmqctl add_vhost myvhost
+$ sudo rabbitmqctl set_user_tags myuser mytag
+$ sudo rabbitmqctl set_permissions -p myvhost myuser ".*" ".*" ".*"
+
+# Details about RabbitMQ
+current node details:
+- node name: 'rabbitmq-cli-86@lamine'
+- home dir: /var/lib/rabbitmq
+- cookie hash: t95PIM9vnvqAuUzA4/pYgg==
+
+Useful link : http://docs.celeryproject.org/en/latest/getting-started/brokers/index.html#broker-instructions
