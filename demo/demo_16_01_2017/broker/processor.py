d9ca762e52e333e9c11b9bd707526309e15e6b15
Lamine <lemzoba@gmail.com>
2017-01-16 14:47:24 +0100

    updating the processor

--- demo/demo_16_01_2017/broker/processor.py
+++ demo/demo_16_01_2017/broker/processor.py
@@ -8,6 +8,6 @@ def callback(channel, method, properties, body):
     msg_context = json.loads(decoded_msg)
     print(" [x] Traitement du dossier de ... {0} {1} ..."
           .format(msg_context['nom'], msg_context['prenom']))
-    time.sleep(1)
+    time.sleep(5)
     channel.basic_ack(delivery_tag=method.delivery_tag)
     channel.stop_consuming()
