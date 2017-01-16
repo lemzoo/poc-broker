4036f1307023b950196c23e9c696bfb56e660a5f
Lamine <lemzoba@gmail.com>
2017-01-16 12:45:23 +0100

    Arranging the data to send to comply json structure

--- demo/demo_16_01_2017/publisher.py
+++ demo/demo_16_01_2017/publisher.py
@@ -8,6 +8,7 @@ data = [{
         "nom": "Dupont",
         "prenom": "Jean"
         },
+
         {
         "id": "10000",
         "nom": "Blanc",
@@ -19,36 +20,43 @@ data = [{
         "nom": "Karhamazov",
         "prenom": "Dimitry"
         },
+
         {
         "id": "30000",
         "nom": "Sorel",
         "prenom": "Julien"
         },
+
         {
         "id": "40000",
         "nom": "Valjean",
         "prenom": "Jean"
         },
+
         {
         "id": "50000",
         "nom": "Vian",
         "prenom": "Boris"
         },
+
         {
         "id": "60000",
         "nom": "Javert",
         "prenom": "Jean"
         },
-                {
+
+        {
         "id": "70000",
         "nom": "Martin",
         "prenom": "Paul"
         },
+
         {
         "id": "90000",
         "nom": "Camus",
         "prenom": "Albert"
-        }]
+        }
+        ]
 
 if __name__ == '__main__':
     publisher = Producer()
