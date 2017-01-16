da3e11bb0ce3570d0be4b9eb6398aa371c07f8a8
Lamine <lemzoba@gmail.com>
2017-01-16 14:30:48 +0100

    Last update for the demo

--- demo/demo_16_01_2017/broker/manage_worker.py
+++ demo/demo_16_01_2017/broker/manage_worker.py
@@ -24,56 +24,27 @@ class ManageWorker(Thread):
         self.user_id = user_id
         self.password = password
 
-    def launch_manager(self):
-        process_time = 2  # 2 secondes
-
-        # Get all existing queues and check if the queue args exists
-        queues = get_all_queue(self.host, self.port,
-                               self.user_id, self.password)
-
-        number_queues = len(queues)
-        print('Nombre de dossier à traiter : %s' % number_queues)
-
-        for queue in queues:
-            # Init a new worker to job on the queue
-            worker = Worker()
-            # Make a lock until the worker to do all the job about the msg
-            with lock:
-                try:
-                    worker.start_consuming(queue)
-                except KeyboardInterrupt:
-                    worker.stop_consuming()
-
-        if number_queues == 0:
-            process_time = 5
-        else:
-            process_time = 2
-        time.sleep(process_time)
-
     def run(self):
-        print('... Démarage du broker de message ...')
-        process_time = 1  # 2 secondes
+        print('... Demarage du broker de message ...')
+        process_time = 2  # 2 secondes
         while True:
             # Get all existing queues and check if the queue args exists
             queues = get_all_queue(self.host, self.port,
                                    self.user_id, self.password)
 
             number_queues = len(queues)
-            print('Nombre de dossier à traiter : %s' % number_queues)
+            print('Nombre de dossier a traiter : %s' % number_queues)
 
             for queue in queues:
+                process_time = 2
                 # Init a new worker to job on the queue
                 worker = Worker()
+
                 # Make a lock until the worker to do all the job about the msg
                 with lock:
-                    try:
-                        worker.start_consuming(queue)
-                    except KeyboardInterrupt:
-                        worker.stop_consuming()
-                    worker.close_connection()
+                    worker.start_consuming(queue)
+
+            time.sleep(process_time)
 
             if number_queues == 0:
                 process_time += 0.5
-            else:
-                process_time = 1
-            time.sleep(process_time)
