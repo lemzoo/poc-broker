cd283a54803886035acd579af6428c8080524838
Thoams Luquet <thomas@luquet.net>
2017-01-24 16:58:07 +0100

    small update

--- implementation/consumer.py
+++ implementation/consumer.py
@@ -1,9 +1,15 @@
-from broker.manage_worker import ManageWorker
+from broker.worker import Worker
+from broker.worker import logging, LOG_FORMAT
+
+
+def main() :
+    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+    example = Worker('amqp://guest:guest@localhost:5672/%2F')
+    try:
+        example.run()
+    except KeyboardInterrupt:
+        example.stop()
 
 
 if __name__ == '__main__':
-    # Create Manager
-    manager = ManageWorker()
-
-    # Run the manager
-    manager.run()
+    main()
