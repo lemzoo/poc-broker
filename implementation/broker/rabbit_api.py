95c418f76e159ff34c32588b2e698d8895004a0c
Lamine <lemzoba@gmail.com>
2017-01-24 16:02:12 +0100

    Adding a method to list all exsiting queue on rabbit by using pyrabbit li

--- implementation/broker/rabbit_api.py
+++ implementation/broker/rabbit_api.py
@@ -1,4 +1,5 @@
 import requests
+from pyrabbit.api import Client
 
 
 def get_all_queue(host='localhost', port=15672,
@@ -7,3 +8,11 @@ def get_all_queue(host='localhost', port=15672,
         response = requests.get(url, auth=(user_id, password))
         queues = [q['name'] for q in response.json()]
         return queues
+
+
+def list_queues(host='localhost', port=15672,
+                user_id='guest', password='guest'):
+    url = '%s:%s' % (host, port)
+    client = Client(url, user_id, password)
+    queues = [q['name'] for q in client.get_queues()]
+    return queues
