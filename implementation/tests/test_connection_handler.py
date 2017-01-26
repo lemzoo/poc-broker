a4f14ec67076cb8368b2aea6013b95ff8ef9470c
sahamada <saidi.ahamada@gmail.com>
2017-01-26 18:42:05 +0100

    update with pytest

--- implementation/tests/test_connection_handler.py
+++ implementation/tests/test_connection_handler.py
@@ -1,25 +1,28 @@
-import unittest
-import sys
-import pika
+import pytest
 
-sys.path.append('/vagrant/code/poc-broker/implementation/broker')
+from pika import BlockingConnection
+from pika.exceptions import ConnectionClosed
+
 from broker.connection_handler import ConnectionHandler
 
-
-class TestConnnectionHandler(unittest.TestCase):
+class TestConnnectionHandler():
 
     def test_init_app(self):
         connection_handler = ConnectionHandler()
-        self.assertIs(True,connection_handler._connection.is_open)
-        self.assertIsInstance(connection_handler._connection, pika.BlockingConnection)
-        self.assertIsNotNone(connection_handler._connection)
+        assert True == connection_handler._connection.is_open
+        assert isinstance(connection_handler._connection, BlockingConnection)
+        assert connection_handler._connection is None
+
+    def test_open_connection_with_bad_credential(self):
+        with pytest.raises(ConnectionClosed):
+             connection_handler =  ConnectionHandler('localhost',5555,'guest','guest')
 
     def test_get_connection(self):
         connection_handler = ConnectionHandler()
-        self.assertIs(True, connection_handler._connection.is_open)
-        self.assertIsNotNone(connection_handler._connection)
+        assert True == connection_handler._connection.is_open
+        assert connection_handler._connection is not None
 
     def test_close_connection(self):
         connection_handler = ConnectionHandler()
         connection_handler.close_connection()
-        self.assertIs(True,connection_handler._connection.is_closed)
+        assert True == connection_handler._connection.is_closed
