942d9b93d24d06a2247b0e39a26aaafd447d0997
Lamine <lemzoba@gmail.com>
2017-01-26 17:44:06 +0100

    Implementating methods for the consumer for channel handler

--- implementation/broker/channel_handler.py
+++ implementation/broker/channel_handler.py
@@ -63,3 +63,84 @@ class ChannelHandler(object):
         if self._channel is None:
             raise ChannelDoesntExist('The channel doesn''t exist yet')
         return self._channel
+
+    def consume_message_on_queue(self, queue):
+        """This method sets up the consumer by first calling
+        add_on_cancel_callback so that the object is notified if RabbitMQ
+        cancels the consumer. It then issues the Basic.Consume RPC command
+        which returns the consumer tag that is used to uniquely identify the
+        consumer with RabbitMQ. We keep the value to use it when we want to
+        cancel consuming. The on_message method is passed in as a callback pika
+        will invoke when a message is fully received.
+
+        """
+        LOGGER.info('Consuming message on queue : %s', queue)
+        self.add_on_cancel_callback()
+
+        try:
+            """The queue can be non exist on rabbit, so ChannelClosed exception
+            is handled by RabbitMQ and then the TCP connection is closed.
+            Re-implement this if others worker can be launch and handle the
+            Exception.
+            """
+            # Get a basic single message
+            basic_deliver, properties, body = self._channel.basic_get(queue)
+            if basic_deliver is None or basic_deliver.NAME == 'Basic.GetEmpty':
+                # Nothing to do when no message
+                pass
+            else:
+                self.on_message(basic_deliver, properties, body)
+
+        except Exception as e:
+            LOGGER.error('The Channel was closed. Please open the connection')
+            return
+
+    def on_message(self, basic_deliver, properties, body):
+        # def on_message(self, body):
+        """Invoked by pika when a message is delivered from RabbitMQ. The
+        channel is passed for your convenience. The basic_deliver object that
+        is passed in carries the exchange, routing key, delivery tag and
+        a redelivered flag for the message. The properties passed in is an
+        instance of BasicProperties with the message properties and the body
+        is the message that was sent.
+
+        :param pika.Spec.Basic.Deliver: basic_deliver method
+        :param pika.Spec.BasicProperties: properties
+        :param str|unicode body: The message body
+
+        """
+        # LOGGER.info('Received message # %s from %s: %s',
+        #            basic_deliver.delivery_tag, properties.app_id, body)
+        LOGGER.info('Received message # %s #', body)
+        self.acknowledge_message(basic_deliver.delivery_tag)
+
+    def acknowledge_message(self, delivery_tag):
+        """Acknowledge the message delivery from RabbitMQ by sending a
+        Basic.Ack RPC method for the delivery tag.
+
+        :param int delivery_tag: The delivery tag from the Basic.Deliver frame
+
+        """
+        LOGGER.info('Acknowledging message %s', delivery_tag)
+        self._channel.basic_ack(delivery_tag)
+
+    def add_on_cancel_callback(self):
+        """Add a callback that will be invoked if RabbitMQ cancels the consumer
+        for some reason. If RabbitMQ does cancel the consumer,
+        on_consumer_cancelled will be invoked by pika.
+
+        """
+        LOGGER.info('Adding consumer cancellation callback')
+        self._channel.add_on_cancel_callback(self.on_consumer_cancelled)
+
+    def on_consumer_cancelled(self, method_frame):
+        """Invoked by pika when RabbitMQ sends a Basic.Cancel for a consumer
+        receiving messages.
+
+        :param pika.frame.Method method_frame: The Basic.Cancel frame
+
+        """
+        LOGGER.info('Consumer was cancelled remotely, shutting down: %r',
+                    method_frame)
+        if self._channel:
+            self._channel.close()
